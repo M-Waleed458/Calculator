75846c1b7680bb0b6ee5ef9646cd8e1468564e88
M-Waleed <muhammadwaleed1376@gmail.com>
2025-04-12 05:36:40 -0700

    greeting

--- sum.py
+++ sum.py
@@ -4,6 +4,7 @@ b : int = 43
 print(f'Sum of numbers is : {a+b}') 
 average : float = (a+b) / 2
 print(f'average is {average}')
+print("hello how are you , what are you doing?")
 
 print(a>b)
 print(a<b)
